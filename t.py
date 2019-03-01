dc7535af4ac68743db9fe3c6a04f3620bc48b7f0
Jonathan Findley <jon.f@technologist.com>
2019-03-01 15:25:32 -0600

    Added a file that needs formatting

---
+++ t.py
@@ -0,0 +1,5 @@
+import json
+import os
+def whitespace_issues():
+    x = 'should be double quotes'
+    return "this line is too long                                                                                        "
