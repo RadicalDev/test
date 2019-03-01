75519978ab556b7fadf961c79969a1f71a5c268e
Jonathan Findley <jon.f@technologist.com>
2019-03-01 15:57:31 -0600

    don't need this anymore

--- t.py
+++
@@ -1,5 +0,0 @@
-import json
-import os
-def whitespace_issues():
-    x = 'should be double quotes'
-    return "this line is too long                                                                                        "
